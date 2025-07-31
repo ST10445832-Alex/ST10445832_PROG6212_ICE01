94ae10e7134f8a33798b33c0ce6299828b3d11d6
Al-Eduardo <st10445832@vcconnect.edu.za>
2025-07-31 14:38:47 +0200

    Initial Commit

---
+++ CustomIndexer/NameList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace CustomIndexer
+{
+    internal class NameList
+    {
+        private List<string> _names = new List<string>();
+
+        public NameList()
+        {
+            _names.Add("Anna");
+            _names.Add("Bob");
+        }
+
+        public string this[int index]
+        {
+            get => _names[index];
+            set
+            {
+                if (index >= 0 && index < _names.Count)
+                {
+                    _names[index] = value;
+                }
+                else if (index == _names.Count)
+                {
+                    _names.Add(value);
+                }
+                else
+                {
+                    throw new System.IndexOutOfRangeException("Index out of bounds.");
+                }
+            }
+        }
+    }
+}
---
+++ CustomIndexer/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace CustomIndexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var namesList = new NameList();
+            Console.WriteLine(namesList[0]);
+
+            Console.WriteLine(namesList[1]);
+            namesList[2] = "Thomas";
+            Console.WriteLine(namesList[2]);
+
+            namesList[0] = "James";
+            Console.Write(namesList[0]);
+        }
+    }
+}
